7402c1e1da433c0ed147156496871d40f18f9156
sanjidaprokriti <sanjidasau79@gmail.com>
2025-05-07 23:00:25 +0600

    Day 2 Practice

---
+++ RDSML Day-4-R Programming Basics (part 2)/rbasics(Part 2).R
@@ -0,0 +1,51 @@
+# Data Types in R
+# 1.Numeric-Integer/Floating Point
+# 2.String-"sANJIDA"/'sANJIDA'
+# 3.LogicaL
+
+a = 11
+b =7.19
+class(a)
+class(b)
+
+name ="Sanjida Abedin"
+print(name)
+class(name)
+
+c ="52"
+print(c+9)
+print(a+9)
+class(c)
+class(c)
+class(c)
+
+
+# Logical Data Type-True,False (T,F
+
+a > b
+class(a > b)
+print (a > b)
+x = 19
+y =22-3
+print(x == y)
+print(x != y)
+print(y > x)
+print(y >=x)
+
+
+# Logical operators In R
+# && Logical Operator
+# || Logical Operator
+4 == 4 && 5 == 5
+x != y || x == y
+
+# Simple mathematical Operator in R
+z = 6 + 9 - 8 * (2 / 3)
+print(z)
+w = (4 + 3 - 3) * 2 / 3
+print(w)
+y = 4^3
+print(y)
+
+11 %% 5
+11 %/% 5
