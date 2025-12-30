42c9f582e60cc0654a41a22cc7b32494558d39b4
sanjidaprokriti <sanjidasau79@gmail.com>
2025-12-30 23:58:05 +0600

    Create .rHISTORY.R

---
+++ RDSML-Day-20 Styling Plots for Publications/.rHISTORY.R
@@ -0,0 +1,8 @@
+5+6
+setwd("D:/R/R-for-Data-Science-and-Machine-Learning-with-NBICT-LAB-Batch-8/RDSML-Day-2 An Introduction to GitHub")
+# Upadating the file
+data = c(9, 6, 17, 31, 11)
+x = mean(data)
+summary(data)
+z = x/2
+z
