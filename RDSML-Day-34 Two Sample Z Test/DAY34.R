91c0a6802bb330ef56bb229680ec2df45d43e90f
sanjidaprokriti <sanjidasau79@gmail.com>
2025-12-31 12:26:01 +0600

    Create DAY34.R

---
+++ RDSML-Day-34 Two Sample Z Test/DAY34.R
@@ -0,0 +1,12 @@
+# Two sample Z test
+# Importing the library
+library(BSDA)
+
+# Importing the dataset
+dataset <- read.csv("Perfume_Two_Sample.csv")
+
+machine1sd <- sd(dataset$Machine.1)
+machine2sd <- sd(dataset$Machine.2)
+
+# Running the z test
+z.test(x = dataset$Machine.1, y = dataset$Machine.2, sigma.x = machine1sd, sigma.y = machine2sd)
