eb32fe13f6f7a878a9ade72102b75ebf14044606
sanjidaprokriti <sanjidasau79@gmail.com>
2025-12-31 14:06:11 +0600

    Create multi_1.R

---
+++ RDSML-Day-49 Multiple Linear Regression in R (Part-2)/multi_1.R
@@ -0,0 +1,23 @@
+Multiple Linear Regression
+
+# Importing the dataset 
+dataset <- read.csv ("50_Startups.csv")
+
+# Encoding the catagorical data
+dataset$State <- factor(dataset$State,
+                        levels = c('New York', 'California', 'Florida'),
+                        labels = c(1,2,3))
+
+# Spliting the dataset into training and test set
+library(caTools)
+set.seed(123)
+split <- sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set <- subset(dataset, split == TRUE)
+test_set <- subset(dataset, split == FALSE)
+
+# Fitting Multiple Linear Regression to the Training Set
+regressor <- lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State, 
+                data = training_set)
+summary(regressor)
+
+# Predicting the Test set res
